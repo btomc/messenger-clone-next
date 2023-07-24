5e4900335484dc33d6a549a6c4d040d474c2e146
Beata Tomczak <tma.btty@gmail.com>
2023-07-24 19:16:37 +0200

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Messenger',
+  title: 'Messenger Clone',
   description: 'Messenger next',
 }
 
